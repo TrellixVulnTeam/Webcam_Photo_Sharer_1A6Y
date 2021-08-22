901aa945314e5090380a185fa7ce8fbf95a41db2
h1m <himanshu.yadav643@gmail.com>
2021-08-22 20:57:23 +0530

    getting the text from the text box

--- main.py
+++ main.py
@@ -2,11 +2,15 @@ from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
 
+import wikipedia
+import requests
+
 Builder.load_file("frontend.kv")
 
 
 class FirstScreen(Screen):
     def search_image(self):
+        query = self.manager.current_screen.ids.user_query.text
         self.manager.current_screen.ids.img.source = "images/down.png"
 
 
