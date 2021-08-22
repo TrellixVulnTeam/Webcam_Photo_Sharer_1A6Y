cfac15826b3826bd98b42c5fcf4c7ae2695868fc
h1m <himanshu.yadav643@gmail.com>
2021-08-22 21:08:07 +0530

    able to search an image and show it to the screen

--- main.py
+++ main.py
@@ -11,7 +11,14 @@ Builder.load_file("frontend.kv")
 class FirstScreen(Screen):
     def search_image(self):
         query = self.manager.current_screen.ids.user_query.text
-        self.manager.current_screen.ids.img.source = "images/down.png"
+        headers = {'User-agent': 'Mozilla/5.0'}
+        page = wikipedia.page(query)
+        image_links = page.images[0]
+        req = requests.get(image_links, headers=headers)
+        image_path = "images/image.jpg"
+        with open(image_path, "wb") as file:
+            file.write(req.content)
+        self.manager.current_screen.ids.img.source = image_path
 
 
 class RootWidget(ScreenManager):
